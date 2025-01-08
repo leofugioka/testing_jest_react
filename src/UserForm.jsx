ff9cb9ca18841a160d7432554c90044df73d338a
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-01-08 15:57:19 -0300

    modified App file and added components for UserForm and UserList

---
+++ src/UserForm.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function UserForm({ onUserAdd }) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const onNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const onEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    onUserAdd({ name, email });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" value={name} onChange={onNameChange} />
+      </div>
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" value={email} onChange={onEmailChange} />
+      </div>
+      <button>Add User</button>
+    </form>
+  );
+}
+
+export default UserForm;
