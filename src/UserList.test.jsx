5a563b2b7060c1a9c1e6e1610e4011c9aca55344
Leandro Fugioka <77177218+leofugioka@users.noreply.github.com>
2025-01-09 14:18:38 -0300

    Finished UserList testing

--- src/UserList.test.jsx
+++ src/UserList.test.jsx
@@ -1,15 +1,39 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
-  // render component
+function createUsers() {
   const users = [
     { name: "joão", email: "joao@joao.com" },
     { name: "laura", email: "laura@laura.com" },
   ];
-  render(<UserList />);
+  return { users };
+}
+
+test("render one row per user", () => {
+  // render component
+  const { users } = createUsers();
+  const { container } = render(<UserList users={users} />);
 
   // find all the rows in the table
+  // A work around to avoid header's row:
+  const rows = container.querySelectorAll("tbody tr");
+  // const rows = within(screen.getByTestId("users")).getAllByRole("row"); // requires to add data-testid into component, may not be ideal depending on your company's directives
+  // const rows = screen.getAllByRole("row"); // not ideal because table header's row is also accounted as a row
+  // screen.logTestingPlaygroundURL(); useful helper to build testing queries
 
   // Assertion: correct number of rows in the table, 1 for each user
+  expect(rows).toHaveLength(2);
+});
+
+test("to have a row for each user", () => {
+  const { users } = createUsers();
+  // render component
+  render(<UserList users={users} />);
+
+  // find rows for each user
+  const rowJoao = screen.getByRole("row", { name: /joão joao@joao\.com/i });
+  const rowLaura = screen.getByRole("row", { name: /laura laura@laura\.com/i });
+
+  expect(rowJoao).toBeInTheDocument();
+  expect(rowLaura).toBeInTheDocument();
 });
